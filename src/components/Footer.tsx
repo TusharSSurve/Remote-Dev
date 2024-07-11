d9313d29b84b1fae109024912e95ea4ab90cd6ec
TusharSSurve <ttusharsurve567@gmail.com>
2024-07-11 22:49:27 +0530

    [main] : Basic Layout Added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,9 @@ export default function Footer() {
       <small>
         <p>
           © Copyright by{" "}
-          <a href="https://bytegrad.com" target="_blank">
-            ByteGrad.com
+          <a href="https://tusharssurve.github.io/portfolio/" target="_blank">
+            TusharSurve
           </a>
-          . Intended for learning or your portfolio.
         </p>
       </small>
 
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState([]);
+
+  useEffect(() => {
+    if (!searchText) return;
+    fetch(`https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`).then(res => {
+      if (!res.ok) {
+        return
+      }
+      return res.json()
+    }).then(data => {
+      setJobItems(data.jobItems);
+    })
+  }, [searchText]);
   return (
-    <form action="#" className="search">
+    <form action="#" className="search" onSubmit={(e) => e.preventDefault()}>
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
         spellCheck="false"
         type="text"
         required
